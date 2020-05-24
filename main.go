e8e49245110cd622214fa2eb3a83dfc0572f6fde
Wexort <bugwriter@outlook.com>
2020-05-24 13:18:06 +0200

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	localIP := getOutboundIP()
-	version := "1.0.0"
+	version := "1.0.1"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "nihao version %s from %s", version, localIP)
 	})
